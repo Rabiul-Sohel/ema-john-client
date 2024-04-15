334407740b3fda47d31727904ba05f5d6e5edd2c
Rabiul Sohel <rabiulalamsohel@gmail.com>
2024-04-15 19:39:32 +0600

    pagination with skipping button

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -8,12 +8,53 @@ import { Link } from 'react-router-dom';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
+    const [totalPages, setTotalPages] = useState(0)
+    const [currentPage, setCurrentPage]=useState(1)
+    const [limit, setLimit] = useState(10)
+    // console.log(totalPages);
+    const maxVisibleButton = 6;
+
+
+    const buttons =[];
+    // for(let i = 1; i <= totalPages; i++){
+    //     buttons.push(i)
+    // }
+    if(totalPages > maxVisibleButton){
+        if(currentPage <= 4){
+           for(let i = 1; i <= maxVisibleButton-2; i++){
+            buttons.push(i)
+           }
+           buttons.push('...')
+           buttons.push(totalPages)
+        } else if(currentPage >= totalPages-3){
+            buttons.push(1)
+            buttons.push('...')
+            for(let i = totalPages-(maxVisibleButton-3); i <= totalPages; i++){
+                buttons.push(i)
+            }
+        } else {
+            buttons.push(1)
+            buttons.push('...')
+            buttons.push(currentPage -1)
+            buttons.push(currentPage)
+            buttons.push(currentPage + 1)
+            buttons.push('...')
+            buttons.push(totalPages)
+        }
+    } else {
+        for(let i = 1; i <= totalPages; i++){
+                buttons.push(i) 
+}}
 
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch(`http://localhost:5000/productsPage?page=${currentPage}&limit=${limit}`)
             .then(res => res.json())
-            .then(data => setProducts(data))
-    }, []);
+            .then(data => {
+                console.log(data);
+                setProducts(data.data)
+                setTotalPages(data.totalPages)
+            })
+    }, [currentPage, limit]);
 
     useEffect(() => {
         const storedCart = getShoppingCart();
@@ -60,6 +101,20 @@ const Shop = () => {
         setCart([]);
         deleteShoppingCart();
     }
+    const handlePrevPage =()=>{
+        if(currentPage > 0){
+            setCurrentPage(currentPage - 1)
+        }
+    }
+    const handleNextPage = ()=>{
+        if(currentPage < totalPages){
+            setCurrentPage(currentPage + 1)
+        }
+    }
+    const handleProductLimit = e =>{
+        const value = e.target.value;
+        setLimit(value)
+    }
 
     return (
         <div className='shop-container'>
@@ -81,6 +136,20 @@ const Shop = () => {
                         <button className='btn-proceed'>Review Order</button>
                     </Link>
                 </Cart>
+            </div>
+        
+            <div style={{margin: '20px auto'}}>
+                <button onClick={handlePrevPage}>Prev</button>
+                {
+                    buttons.map((btn, idx)=> <button style={currentPage === btn ? {backgroundColor:'red'}:undefined} onClick={()=> setCurrentPage(btn)} key={idx}> {btn} </button> )
+                }
+                <button onClick={handleNextPage}>Next</button>
+                <select style={{padding:'10px'}} defaultValue='10' onChange={handleProductLimit} name="" id="">
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                </select>
             </div>
         </div>
     );
